78ab290d46c89ab18c619e4f8efc54d2f44f6866
thetkhine <macbook@MacBooks-MacBook-Pro.local>
2025-03-08 20:21:45 +0630

    add SQL

--- JavaSE6thBatch/JavaSE6thBatch/src/main/java/com/turing/javase6/chapter32/CheckboxDemo.java
+++ JavaSE6thBatch/JavaSE6thBatch/src/main/java/com/turing/javase6/chapter32/CheckboxDemo.java
@@ -4,6 +4,8 @@
  */
 package com.turing.javase6.chapter32;
 
+import java.awt.Checkbox;
+import java.awt.CheckboxGroup;
 import java.awt.FlowLayout;
 import javax.swing.JCheckBox;
 import javax.swing.JLabel;
@@ -17,14 +19,36 @@ public class CheckboxDemo extends CommonFrame{
     @Override
     public void initComponent() {
         this.setLayout(new FlowLayout());
-        JLabel lblCheck = new JLabel("Check");
-        this.add(lblCheck);
         
-        JCheckBox chkCheck = new JCheckBox();
-        this.add(chkCheck);
+        CheckboxGroup cbGroup = new CheckboxGroup();
+        
+        JLabel lblMale = new JLabel("Male");
+        //this.add(lblMale);
+        
+        Checkbox chkMale = new Checkbox("Male ",cbGroup,false);
+        this.add(chkMale);
+        
+        chkMale.addItemListener(eve->{
+            System.out.println("chkMale click "+eve.getStateChange());
+        });
+         
+        JLabel lblFeMale = new JLabel("Female");
+        //this.add(lblFeMale);
+        
+        Checkbox chkFemale = new Checkbox("Female",cbGroup,false);
+        this.add(chkFemale);
+        
+        chkFemale.addItemListener(eve->{
+            System.out.println("chkFemale click "+eve.getStateChange());
+        });
     }
     public static void main(String[] args) {
-        new CheckboxDemo();
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                 new CheckboxDemo().setVisible(true);
+            }
+        });
+       
     }
     
 }
